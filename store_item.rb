164472437cf43f184a3ea5727e692dd1ac6725c7
therobgeorge <georg1rd@gmail.com>
2021-04-28 15:32:16 -0400

    converts to class w/reader/writers

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,40 @@
-shirt1 = {:type => "crew neck", :size => "M", :color => "Black"}
-shirt2 = {type: "hoodie", size: "S", color: "Green"}
-shirt3 = {type: "western", size: "L", color: "blue"}
+# shirt1 = {:type => "crew neck", :size => "M", :color => "Black"}
+# shirt2 = {type: "hoodie", size: "S", color: "Green"}
+# shirt3 = {type: "western", size: "L", color: "blue"}
+
+
+class Shirt 
+  
+  attr_accessor :type, :size, :color
+
+  def initialize(type, size, color)
+    @type = type
+    @size = size
+    @color = color    
+  end
+  # def type
+  #   @type    
+  # end
+  # def size
+  #   @size
+  # end
+  # def color
+  #   @color
+  # end
+  # def type=(input_type)
+  #   @type = input_type
+  # end
+  # def size=(input_size)
+  #   @size = input_size    
+  # end
+  # def color=(input_color)
+  #   @color = input color
+  # end
+end
+
+crew_shirt = Shirt.new("crew neck", "M", "Black")
+puts crew_shirt.type
+puts crew_shirt.size
+puts crew_shirt.color
+crew_shirt.size = "L"
+puts crew_shirt.size
